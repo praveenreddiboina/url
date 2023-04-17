111a959e1554d8cd0f5b96b86d16b38a1fc7e1eb
praveenreddiboina <126179493+praveenreddiboina@users.noreply.github.com>
2023-04-17 17:47:59 +0530

    coments

--- urlpro/urlApp/views.py
+++ urlpro/urlApp/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render, redirect
-import uuid
+import uuid ##If all you want is a unique ID, you should probably call uuid1() or uuid4(). Note that uuid1() may compromise privacy since it creates a UUID containing the computer's network address. uuid4() creates a random UUID.
 from .models import Url
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
+def index(request):##rendering index.html file
     return render(request, 'index.html')
 
 def create(request):
-    if request.method == 'POST':
-        link = request.POST['link']
-        uid = str(uuid.uuid4())[:5]
-        new_url = Url(link=link,uuid=uid)
-        new_url.save()
-        return HttpResponse(uid)
+    if request.method == 'POST': # #cheking method is post are not. 
+        link = request.POST['link']  #accesing the input long url from user.
+        uid = str(uuid.uuid4())[:5] # creating a ranadam id(by using univeselly unique identifier(uuid) module).
+        new_url = Url(link=link,uuid=uid)  # creating url in data base as 2 argumentss link as long url uuid as new url  
+        new_url.save() # saving the new url in data base
+        return HttpResponse(uid) # returning uid(short url)
     
 def go(request, pk):
     url_details = Url.objects.get(uuid=pk)
