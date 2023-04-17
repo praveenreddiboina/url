27afc7c4b33b206a9d497da98f31482ca2976270
praveenreddiboina <126179493+praveenreddiboina@users.noreply.github.com>
2023-04-17 17:48:35 +0530

    comments

--- urlpro/urlApp/models.py
+++ urlpro/urlApp/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class Url(models.Model):
+class Url(models.Model): # creating class and giving it two objects as follows.
     link = models.CharField(max_length=10000)
     uuid = models.CharField(max_length=10)
